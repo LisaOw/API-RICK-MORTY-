fc8601858bf468f4eaeb9f0e4a77471b658518ab
LisaOw <lisaow2023@gmail.com>
2024-05-30 15:34:41 +0700

    Second Commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
             <div className="bg-lightBlack bg-cover min-h-screen px-10 py-12 lg:p-20">
                     <CharacterList className=" w-full container mx-auto" />
             </div>
+            <div>
+                <p>Hey wassup</p>
+            </div>
         </div>
 
     )
